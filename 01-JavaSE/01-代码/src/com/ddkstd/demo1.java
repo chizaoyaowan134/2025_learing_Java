ea16e0f7b7593d8dbfea54ffc0d03ad5b125c5cf
chizaoyaowan134 <1498431730@qq.com>
2025-03-17 12:07:36 +0800

    day1

---
+++ 01-JavaSE/01-代码/src/com/ddkstd/demo1.java
@@ -0,0 +1,18 @@
+package com.ddkstd;
+
+/**
+ * ClassName: demo1
+ * Package: com.ddkstd
+ * Description:
+ *
+ * @Author： 吃枣药丸
+ * @Create: 2025/3/12 - 19:20
+ * @Version: v1.0
+ */
+public class demo1 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
